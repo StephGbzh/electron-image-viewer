40ffb9e3fcb61d58baae0602f853e3877d342fb4
StephGbzh <username@users.noreply.github.com>
2017-07-29 17:25:35 +0200

    Replace Foreman + Procfile + js script wait with Concurrently + Wait-On

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -17,7 +17,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     //mainWindow.loadURL('http://localhost:3000');
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
+    const startUrl = process.env.ELECTRON_DEV ? "http://localhost:3000/" : url.format({
             pathname: path.join(__dirname, '/../build/index.html'),
             protocol: 'file:',
             slashes: true
